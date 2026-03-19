801b181a772bf17ad5710ad9359c2f92c0aaaf38
kalariashubh <kalariashubh@gmail.com>
2026-03-19 15:51:10 +0530

    Update config.py

--- rebar_aps_extractor/config.py
+++ rebar_aps_extractor/config.py
@@ -1,8 +1,10 @@
 import os
 
 CLIENT_ID     = "84brt2ueE7l53914PlqeFlg9CU93fqid204pXBguUZzo2wVi"
+# CLIENT_ID     = "Mc7RCA93iFvb4DvYjs0JjTQG828UQdcuKSvl7kKnrLcAdh8w"
 
 CLIENT_SECRET = "IpaF6PSGItMxgLiqtbe1DiqrXqfPgGFDCjFYoisUNJzQwhlkNCElYGEMzOERBbmw"
+# CLIENT_SECRET = "Dhhfw3DDZTjm4AFc1igvJZDfL4tHoJ92eAb8R9njGRZDT8bhdnmaIFLEGRkhVWPG"
 
 RVT_FILE_PATH = r"D:\Buniyad Byte\POC 9\model.rvt"
 
